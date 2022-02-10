283524a0ff94ad3b0732b6540bc878b6d6464cb4
CeeJayyy007 <jamesjamil007@gmail.com>
2022-02-10 10:39:37 +0100

    chore(migration): created new migrations for the app

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->default('other');
+            $table->date('dob')->nullable();
+            $table->integer('age')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
